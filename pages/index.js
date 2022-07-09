c0d237a488e26c973bfb86ac183e26f50b3da3bb
Fadhel Ijlal Falah <fadhel@femaledaily.com>
2022-07-09 19:17:02 +0700

    feat: add color picker

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,20 @@
-import numToWords from "Functions/numToWords";
-import dynamic from "next/dynamic";
+import ParentRect from "Components/parent-rect";
 import Head from "next/head";
-import React, { useState } from "react";
-const Button = dynamic(
-	() => import("Components/button"),
-	{
-		ssr: false,
-	},
-);
-const Input = dynamic(
-	() => import("Components/input"),
-	{
-		ssr: false,
-	},
-);
+import React from "react";
 
-export default function Home() {
-  const [input, setInput] = useState({
-    number: 0,
-    result: "",
-  });
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const result = numToWords(input.number);
-    setInput((prev) => ({ ...prev, result }));
-  };
-
+export default function Colors() {
   return (
     <>
-      <Head>
-        <title>Number to word</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="layout-container">
-        <h1 className="judul">Convert number to words</h1>
-        <form onSubmit={onSubmit}>
-          <Input
-            label="Input number to word"
-            placeholder="Mohon diisi angka"
-            onChange={(value) =>
-              setInput((prev) => ({ ...prev, number: value }))
-            }
-          />
-          <Button active type="submit">
-            Submit
-          </Button>
-        </form>
-        <h1>{input.result}</h1>
-      </div>
-      <style jsx="true">
-        {`
-          .layout-container {
-            flex-grow: 1;
-            margin: 0 auto 30px;
-            max-width: 935px;
-          }
-          .layout-container h1 {
-            font-weight: 400;
-          }
-        `}
-      </style>
+    <Head>
+      <title>Color Picker</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="layout-container" style={{maxWidth: "80vw"}}>
+        <h1>Simple Color Picker</h1>
+      {Array.from({ length: 8 }, (_, idx) => (
+        <ParentRect idx={idx} />
+      ))}
+    </div>
     </>
   );
 }
---
+++ pages/number.js
@@ -0,0 +1,66 @@
+import numToWords from "Functions/numToWords";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import React, { useState } from "react";
+const Button = dynamic(
+	() => import("Components/button"),
+	{
+		ssr: false,
+	},
+);
+const Input = dynamic(
+	() => import("Components/input"),
+	{
+		ssr: false,
+	},
+);
+
+export default function Home() {
+  const [input, setInput] = useState({
+    number: 0,
+    result: "",
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const result = numToWords(input.number);
+    setInput((prev) => ({ ...prev, result }));
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Number to word</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="layout-container">
+        <h1 className="judul">Convert number to words</h1>
+        <form onSubmit={onSubmit}>
+          <Input
+            label="Input number to word"
+            placeholder="Mohon diisi angka"
+            onChange={(value) =>
+              setInput((prev) => ({ ...prev, number: value }))
+            }
+          />
+          <Button active type="submit">
+            Submit
+          </Button>
+        </form>
+        <h1>{input.result}</h1>
+      </div>
+      <style jsx="true">
+        {`
+          .layout-container {
+            flex-grow: 1;
+            margin: 0 auto 30px;
+            max-width: 935px;
+          }
+          .layout-container h1 {
+            font-weight: 400;
+          }
+        `}
+      </style>
+    </>
+  );
+}
